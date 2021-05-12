c87ed56627b27abb588362f9422bd50363b4b96e
od <od@1234.mn>
2021-05-12 22:09:57 +0800

    Updates

---
+++ precache-manifest.ed728e69579342f0e8562174119e7866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9879354299564afdcbb706f7dbf17636",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "748502098e24d2548bf6",
+    "url": "/burger/static/css/main.82400687.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748502098e24d2548bf6",
+    "url": "/burger/static/js/main.09b1f254.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
